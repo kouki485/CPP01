5868b64e2e8e153429d4570052e03f4e5b83ab54
kouki485 <mwd3145@gmail.com>
2022-12-01 08:30:30 +0900

    sukosi

---
+++ ex00/Zombie.hpp
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Zombie.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kkohki <kkohki@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/01 07:44:59 by kkohki            #+#    #+#             */
+/*   Updated: 2022/12/01 08:24:16 by kkohki           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ZOMBIE_HPP
+#define ZOMBIE_HPP
+
+#include <iostream>
+#include <string>
+#include <iomanip>
+
+
+class Zombie
+{
+	private:
+		std::string name;
+	public:
+		Zombie(){};
+		Zombie(std::string name);
+		~Zombie();
+		void announce(void);
+};
+
+Zombie *newZombie(std::string name);
+void randomChump(std::string name);
+
+#endif
